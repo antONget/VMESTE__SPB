f89497f1b38f33df92927dec2d3b8a7c9835b944
antONget <a.l.ponomare@mail.ru>
2024-09-20 11:42:27 +0300

    git init

---
+++ database/models.py
@@ -0,0 +1,44 @@
+from sqlalchemy import String, Integer
+from sqlalchemy.orm import Mapped, mapped_column, DeclarativeBase
+from sqlalchemy.ext.asyncio import AsyncAttrs, async_sessionmaker, create_async_engine
+
+
+engine = create_async_engine(url="sqlite+aiosqlite:///database/1.sqlite3", echo=False)
+async_session = async_sessionmaker(engine)
+
+
+class Base(AsyncAttrs, DeclarativeBase):
+    pass
+
+
+class User(Base):
+    __tablename__ = 'users'
+
+    id_user: Mapped[int] = mapped_column(primary_key=True)
+    tg_id = mapped_column(Integer)
+    username: Mapped[str] = mapped_column(String(20))
+
+
+class Place(Base):
+    __tablename__ = 'places'
+
+    id_place: Mapped[int] = mapped_column(primary_key=True)
+    title: Mapped[str] = mapped_column(String(20))
+    short_description: Mapped[str] = mapped_column(String(20))
+    long_description: Mapped[str] = mapped_column(String(20))
+    address: Mapped[str] = mapped_column(String(20))
+    instagram: Mapped[str] = mapped_column(String(20))
+    yandex_map: Mapped[str] = mapped_column(String(20))
+    list_image: Mapped[str] = mapped_column(String(20))
+    category: Mapped[str] = mapped_column(String(20))
+    sub_category: Mapped[str] = mapped_column(String(20))
+    count_link: Mapped[int] = mapped_column(Integer)
+    position: Mapped[int] = mapped_column(Integer, default=0)
+    pos_cat: Mapped[int] = mapped_column(Integer, default=0)
+    pos_sub: Mapped[int] = mapped_column(Integer, default=0)
+
+
+async def async_main():
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+
